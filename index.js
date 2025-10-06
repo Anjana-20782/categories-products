b040d6da9c97e6f4d084cd03f12033d6c18a8ceb
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-06 17:24:40 +0530

    fetch products adde

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-async function getData() {
-    try{
-    const response=await fetch("https://dummyjson.com/products");
-    const data=await response.json();
-    console.log("User Data:", data);
+const colors=['#eec2fcff','#c6eef2ff','#c3f9c6ff','#f0baafff'];
 
-    }
-    catch(error){
-
-        console.log("error happenend:",error)
-    }
+async function fetchProduct() {
+  try {
+    const response = await fetch('https://dummyjson.com/products');
+    const data = await response.json();
+    displayProducts(data.products); // reuse helper function
+  } catch (error) {
+    console.log("error happened:", error);
+  }
 }
-getData();
 
+document.getElementById("searchInput").addEventListener("input", async (e) => {
+  const query = e.target.value.toLowerCase();
+  const res = await fetch(`https://dummyjson.com/products/search?q=${query}`);
+  const data = await res.json();
+  displayProducts(data.products);  // now it works ✅
+});
+
+fetchProduct();
+
+
+function displayProducts(products) {
+  const container = document.getElementById('category');
+  container.innerHTML = ""; // clear old results
+
+  products.forEach((e, i) => {
+    const card = document.createElement('div');
+    card.className = 'user-card';
+
+    card.style.backgroundColor = colors[i % colors.length];
+
+    card.innerHTML = `
+      <h3>${e.id}</h3>
+      <h2>${e.title}</h2>
+      <p>${e.description}</p>
+      <h1>${e.category}</h1>
+    `;
+
+    container.appendChild(card);
+  });
+}
